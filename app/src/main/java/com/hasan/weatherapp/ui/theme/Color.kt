1658dcf6255665cfb155311d0a6603d68ff055cf
Hasan Ali <ihasanalihasan@gmail.com>
2025-06-08 15:35:59 +0300

    edit color

--- app/src/main/java/com/hasan/weatherapp/ui/theme/Color.kt
+++ app/src/main/java/com/hasan/weatherapp/ui/theme/Color.kt
@@ -3,11 +3,11 @@ package com.hasan.weatherapp.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val PrimaryColor = Color(0xFF87CEFA)
-val PrimaryColor80 = Color(0xFF87CEFA)
-val Pink80 = Color(0xFFEFB8C8)
+val PrimaryColor80 = Color(0xCC87CEFA)
+val PrimaryColor60 = Color(0x9987CEFA)
 
 val DarkPrimary = Color(0xFF7E2FFF)
-val PurpleGrey40 = Color(0xFF625b71)
+val DarkPrimary80 = Color(0xFF7E2FFF)
 val DarkSecondaryColor = Color(0xFFD4C0FF)
 
 
