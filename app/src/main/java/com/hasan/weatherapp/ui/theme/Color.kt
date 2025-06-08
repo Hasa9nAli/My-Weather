e7eb9704d2adbc00922ccaa9e23b3df132a37039
Hasan Ali <ihasanalihasan@gmail.com>
2025-06-08 15:24:00 +0300

    init commit

---
+++ app/src/main/java/com/hasan/weatherapp/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.hasan.weatherapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryColor = Color(0xFF87CEFA)
+val PrimaryColor80 = Color(0xFF87CEFA)
+val Pink80 = Color(0xFFEFB8C8)
+
+val DarkPrimary = Color(0xFF7E2FFF)
+val PurpleGrey40 = Color(0xFF625b71)
+val DarkSecondaryColor = Color(0xFFD4C0FF)
+
+
+
