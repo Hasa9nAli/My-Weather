1eab451faebfe3577b517857e28a2ab13d6ba86e
Hasan Ali <ihasanalihasan@gmail.com>
2025-06-09 18:04:31 +0300

    edit configuration

--- app/src/main/java/com/hasan/weatherapp/ui/theme/Color.kt
+++ app/src/main/java/com/hasan/weatherapp/ui/theme/Color.kt
@@ -10,8 +10,9 @@ val DarkPrimary = Color(0xFF7E2FFF)
 val DarkPrimary80 = Color(0xFF7E2FFF)
 val DarkSecondaryColor = Color(0xFFD4C0FF)
 
-
+val BlackBold = Color(0xFF323232)
 val BlackColor = Color(0xFF060414)
+val BlackColor87 = Color(0xDE060414)
 val BlackColor80 = Color(0xCC060414)
 val BlackColor60 = Color(0x99060414)
 val BlackColor8 = Color(0x14060414)
