69db5cc7176e4ce7735f2f3d05548dbe3b3d824d
Hasan Ali <ihasanalihasan@gmail.com>
2025-06-11 21:55:33 +0300

    add koin module appModule, viewModelModule, repoModule, networkModule

--- app/src/main/java/com/hasan/weatherapp/di/appModule.kt
+++ app/src/main/java/com/hasan/weatherapp/di/appModule.kt
@@ -1,16 +1,18 @@
 package com.hasan.weatherapp.di
 
+import android.location.Geocoder
+import com.google.android.gms.location.LocationServices
 import com.hasan.weatherapp.data.remote.ApiService
-import com.hasan.weatherapp.data.repository.WeatherRepositoryImpl
-import com.hasan.weatherapp.domain.repository.WeatherRepository
-import com.hasan.weatherapp.viewModel.WeatherViewModel
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import java.util.Locale
 
 val appModule = module {
+
     single {
         HttpClient {
             install(ContentNegotiation) {
@@ -21,11 +23,17 @@ val appModule = module {
             }
         }
     }
+    single {
+        LocationServices.getFusedLocationProviderClient(androidContext())
+    }
+    single {
+        Geocoder(androidContext(), Locale.ENGLISH)
+    }
 
     single { ApiService(get()) }
 
-    single<WeatherRepository> { WeatherRepositoryImpl(get()) }
 
 
-    single { WeatherViewModel(get()) }
+
+
 }
